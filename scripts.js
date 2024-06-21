971e842011715fdda04bf437aea9ae9cae137e10
MriteshAdak <72008709+MriteshAdak@users.noreply.github.com>
2024-06-21 21:00:59 +0530

    added psedocode/blueprint

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
+/*
+# declare variables computerScore and humanScore for storing respective scores and intitilize them to 0
+
+# funciton to get computer's input: getComputerChoice
+    - a random number genertor from 1 to 3 that corresponds to rock, paper and scissor
+    - print the value to make sure the association works
+    - return and store the output in a varaiable: computerChoice
+
+# function to get user's input: getUserChoice
+    - a prompt to get a numerical input from user that associates to the 3 available options
+    - print the value to make sure the assocaiation works
+    - return and store the output in a variable: humanChoice
+
+    # function to play a single round: playRound
+    - two parameters that will have arguements computerChoice and humanChoice
+    - output the winner as a string
+    - increment the variable corresponding to the winner
+
+# function to play best of five rounds: playGame
+    - call the playRound fn five times
+    - based on the final result of the five rounds declare the final winner in console.log
+*/
+
 console.log("Test code")
